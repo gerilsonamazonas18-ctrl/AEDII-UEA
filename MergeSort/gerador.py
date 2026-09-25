b82e442c4c387eac579c1a87864f9395fe850939
gerilsonamazonas18-ctrl <gerilsonamazonas18@gmail.com>
2026-09-25 19:32:46 -0400

    Versão Final

--- MergeSort/gerador.py
+++ MergeSort/gerador.py
@@ -1,9 +1,9 @@
 import random
 
-lista = []
+lista = []   # lista que vai armazenar os números aleatórios
 
-for i in range(0, 100000):
-    lista.append(random.randint(0, 500000))
+for i in range(0, 100000):                    # repete 100.000 vezes
+    lista.append(random.randint(0, 500000))   # gera um número aleatório entre 0 e 500000 e adiciona à lista
 
-with open("arquivo.txt", "w") as f:
-    f.write("\n".join(str(n) for n in lista))
+with open("arquivo.txt", "w") as f:                    # cria (ou sobrescreve) o arquivo "arquivo.txt"
+    f.write("\n".join(str(n) for n in lista))          # escreve os números no arquivo, um por linha
